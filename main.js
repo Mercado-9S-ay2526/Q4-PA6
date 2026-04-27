9e9371ddbd3ec1a398c0c14813144eb47ee72189
Mercado-9S-ay2526 <mercadosancholl@gmail.com>
2026-04-28 07:35:53 +0800

    update main

---
+++ main.js
@@ -0,0 +1,7 @@
+function  countAnimals() {
+    
+    var animals = ["cat1","cat2","cat3","cat4",];
+    for (var i = 0; i < animals.length; i++) {
+    window.alert("This zoo contains a" + animals[i] + ".");
+}
+}
